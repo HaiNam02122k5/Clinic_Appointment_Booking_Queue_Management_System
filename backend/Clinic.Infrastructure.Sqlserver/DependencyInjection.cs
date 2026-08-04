e8ff979fa7c5815f724975dd334dc456ce840cdd
Cuong190105 <cuong19012005@gmail.com>
2026-08-04 14:33:53 +0700

    chore: configure backend base solution and projects

---
+++ backend/Clinic.Infrastructure.Sqlserver/DependencyInjection.cs
@@ -0,0 +1,24 @@
+using Clinic.Domain.Interfaces;
+using Clinic.Infrastructure.Sqlserver.Persistence;
+using Clinic.Infrastructure.Sqlserver.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Clinic.Infrastructure.Sqlserver
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureSqlServer(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            //services.AddScoped<IFarmRepository, FarmRepository>();
+
+            return services;
+        }
+    }
+}
