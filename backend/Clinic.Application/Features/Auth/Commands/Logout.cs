f2b94c17245da4a9017b87a7717fcb22493ff3ba
Cuong190105 <cuong19012005@gmail.com>
2026-08-08 15:39:57 +0700

    feat/fix: add login, fix auth prefix route

---
+++ backend/Clinic.Application/Features/Auth/Commands/Logout.cs
@@ -0,0 +1,28 @@
+﻿using Clinic.Application.Interfaces;
+using MediatR;
+
+namespace Clinic.Application.Features.Auth.Commands
+{
+    public record LogoutCommand(string RefreshToken) : IRequest;
+    public class LogoutCommandHandler : IRequestHandler<LogoutCommand>
+    {
+        private readonly IRefreshTokenRepository _refreshTokenRepository;
+        private readonly ITokenProvider _tokenProvider;
+        public LogoutCommandHandler(IRefreshTokenRepository refreshTokenRepository, ITokenProvider tokenProvider)
+        {
+            _refreshTokenRepository = refreshTokenRepository;
+            _tokenProvider = tokenProvider;
+        }
+
+        public async Task Handle(LogoutCommand request, CancellationToken cancellationToken)
+        {
+            var tokenHash = _tokenProvider.HashToken(request.RefreshToken);
+            var token = await _refreshTokenRepository.GetByTokenHashAsync(tokenHash);
+            if (token !=  null)
+            {
+                token.Revoke();
+                await _refreshTokenRepository.UpdateAsync(token);
+            }
+        }
+    }
+}
