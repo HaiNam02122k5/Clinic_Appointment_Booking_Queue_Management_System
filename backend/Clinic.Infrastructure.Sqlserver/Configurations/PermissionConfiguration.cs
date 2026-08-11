be66deb6e06aad99b92f2ff6f051af50cd7e0ce5
Bùi Đăng Thịnh <166470020+thinhote@users.noreply.github.com>
2026-08-11 10:43:26 +0700

    Add appointment.update permission to configuration

--- backend/Clinic.Infrastructure.Sqlserver/Configurations/PermissionConfiguration.cs
+++ backend/Clinic.Infrastructure.Sqlserver/Configurations/PermissionConfiguration.cs
@@ -28,7 +28,8 @@ namespace Clinic.Infrastructure.Sqlserver.Configurations
                 new Permission(Guid.Parse("10000000-0000-0000-0000-000000000005"), "doctor.view", "View doctor profile", new DateTime(2026, 1, 1), null, false),
                 new Permission(Guid.Parse("10000000-0000-0000-0000-000000000006"), "doctor.edit", "Edit doctor profile", new DateTime(2026, 1, 1), null, false),
                 new Permission(Guid.Parse("10000000-0000-0000-0000-000000000007"), "user.manage", "Manage users", new DateTime(2026, 1, 1), null, false),
-                new Permission(Guid.Parse("10000000-0000-0000-0000-000000000008"), "role.manage", "Manage roles and permissions", new DateTime(2026, 1, 1), null, false)
+                new Permission(Guid.Parse("10000000-0000-0000-0000-000000000008"), "role.manage", "Manage roles and permissions", new DateTime(2026, 1, 1), null, false),
+                new Permission(Guid.Parse("10000000-0000-0000-0000-000000000009"), "appointment.update", "Update appointments", new DateTime(2026, 1, 1), null, false)
             );
         }
     }
