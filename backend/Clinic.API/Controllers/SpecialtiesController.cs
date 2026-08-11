bc3115db5938612da8a2cc4aa4d61b3b719a3cb9
Bùi Đăng Thịnh <166470020+thinhote@users.noreply.github.com>
2026-08-11 16:41:15 +0700

    Change authorization from Roles to Policy for actions

--- backend/Clinic.API/Controllers/SpecialtiesController.cs
+++ backend/Clinic.API/Controllers/SpecialtiesController.cs
@@ -22,21 +22,21 @@ namespace Clinic.API.Controllers
         }
 
         [HttpPost]
-        [Authorize(Roles = "Admin")]
+        [Authorize(Policy = "Permission:specialty.manage")]
         public IActionResult Create()
         {
             return StatusCode(StatusCodes.Status201Created);
         }
 
         [HttpPut("{specialtyId}")]
-        [Authorize(Roles = "Admin")]
+        [Authorize(Policy = "Permission:specialty.manage")]
         public IActionResult Update([FromRoute] string specialtyId)
         {
             return NoContent();
         }
 
         [HttpPatch("{specialtyId}/status")]
-        [Authorize(Roles = "Admin")]
+        [Authorize(Policy = "Permission:specialty.manage")]
         public IActionResult UpdateStatus([FromRoute] string specialtyId)
         {
             return NoContent();
